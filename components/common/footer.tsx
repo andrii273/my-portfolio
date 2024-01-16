56207cf7da803179e75fc65b1f1cbcffc6de2c32
Full Stack Super Guru <95362611+fantasyknight@users.noreply.github.com>
2024-01-16 22:26:20 +0100

    front

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
             target: "_blank",
             rel: "noreferrer",
           }}
-          href="/Andrii_Khorishko.pdf"
+          href="/Marcin.pdf"
         ></Button>
         <Button
           classes="ml-3"
@@ -51,7 +51,7 @@ const Footer = () => {
         ></Button>
       </div>
       <h2 className="text-center text-sm sm:text-base mt-8">
-        Designed and Developed with ❤️ by Andrii
+        Designed and Developed with ❤️ by Marcin
       </h2>
     </>
   );
